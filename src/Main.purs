a30e6dd58ec31f4f3b3942b11cf384b9dbe974dc
Mika Vakula <mika@vakula.fi>
2018-06-30 23:33:14 +0300

    Show latency every second

--- src/Main.purs
+++ src/Main.purs
@@ -2,13 +2,70 @@ module Main where
 
 import Prelude
 
+import Data.Either (Either(..))
+import Effect.Aff (Aff, attempt, launchAff_)
+import Effect.Class (liftEffect)
+import Effect.Class.Console (log, logShow)
+import Effect.Timer (setInterval)
+import Milkis as M
+import Milkis.Impl.Window (windowFetch)
 import React.Basic (ReactComponent, react)
 import React.Basic.DOM as R
+import Simple.JSON (read)
+import Unsafe.Coerce (unsafeCoerce)
+
+
+baseUrl :: String
+baseUrl = "http://localhost:3000/ping/"
+
+url :: String
+url = baseUrl <> "?url=https://google.com"
+
+fetch :: M.Fetch
+fetch = M.fetch windowFetch
+
+type Status =
+  { statusCode :: Int
+  , latency :: Number
+  }
+
+type State = {
+  status :: Status
+}
 
 main :: ReactComponent {}
 main = react { displayName: "Main", initialState, receiveProps, render }
   where
-    initialState = {}
-    receiveProps _ _ _ = pure unit
-    render _ state setState =
-      R.div { children: [ R.text "Hello World" ]}
+    initialState =
+      { status:
+        { statusCode: 0
+        , latency: 0.0
+        }
+      }
+    receiveProps props state setState = launchAff_ do
+      let setState' = liftEffect <<< setState
+      _ <- liftEffect $ setInterval 1000 $ launchAff_ do
+        status <- getPingStatus
+        setState' \s -> s { status = status}
+      pure unit
+
+    render props state setState =
+      R.div { children: [ R.text $ show state.status.latency ]}
+
+getPingStatus :: Aff Status
+getPingStatus = do
+  res <- attempt $ fetch (M.URL url) M.defaultFetchOptions
+  case res of
+    Right response -> do
+      let statusCode = M.statusCode response
+      result <- M.json response
+      case read result of
+        Right (status :: Status) -> do
+          log $ unsafeCoerce status
+          pure status
+        Left e -> do
+          logShow e
+          pure { statusCode: 0, latency: 0.0}
+    Left e -> do
+      logShow e
+      pure { statusCode: 0, latency: 0.0}
