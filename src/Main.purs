ba7ee764305b6ce1a5b596babd1ca021d6632d04
Mika Vakula <mika@vakula.fi>
2018-07-01 01:38:27 +0300

    Show only 30 latest results

--- src/Main.purs
+++ src/Main.purs
@@ -2,6 +2,8 @@ module Main where
 
 import Prelude
 
+import Data.Array ((:))
+import Data.Array as Array
 import Data.Either (Either(..))
 import Effect.Aff (Aff, attempt, launchAff_)
 import Effect.Class (liftEffect)
@@ -24,40 +26,44 @@ url = baseUrl <> "?url=https://google.com"
 fetch :: M.Fetch
 fetch = M.fetch windowFetch
 
-type Status =
+type Ping =
   { statusCode :: Int
   , latency :: Number
   }
 
 type State = {
-  status :: Array Status
+  ping :: Array Ping
 }
 
 main :: ReactComponent {}
 main = react { displayName: "Main", initialState, receiveProps, render }
   where
     initialState =
-      { status: [
+      { ping: [
         { statusCode: 0
         , latency: 0.0
         } ]
       }
     receiveProps props state setState = launchAff_ do
       let setState' = liftEffect <<< setState
-      _ <- liftEffect $ setInterval 1000 $ launchAff_ do
-        status <- getPingStatus
-        setState' \s -> s { status = s.status <> [status]}
+      _ <- liftEffect $ setInterval 1500 $ launchAff_ do
+        ping <- getPingStatus
+        setState' \s -> s { ping = ping : Array.take 30 s.ping }
       pure unit
 
     render props state setState =
       let
-        latencyDiv status = R.div { children: [ R.text $ show status.latency ]}
-        latencies = (\status -> latencyDiv status) <$> state.status
+        latencyDiv ping = R.div
+          { className: "ping-bar"
+          , style: R.css { height: ping.latency / 10.0 }
+          , title: show ping.latency
+          }
+        latencies = (\ping -> latencyDiv ping) <$> state.ping
       in
-        R.div { children: latencies }
+        R.div { className: "pings", children: latencies }
 
 
-getPingStatus :: Aff Status
+getPingStatus :: Aff Ping
 getPingStatus = do
   res <- attempt $ fetch (M.URL url) M.defaultFetchOptions
   case res of
@@ -65,9 +71,9 @@ getPingStatus = do
       let statusCode = M.statusCode response
       result <- M.json response
       case read result of
-        Right (status :: Status) -> do
-          log $ unsafeCoerce status
-          pure status
+        Right (ping :: Ping) -> do
+          log $ unsafeCoerce ping
+          pure ping
         Left e -> do
           logShow e
           pure { statusCode: 0, latency: 0.0}
