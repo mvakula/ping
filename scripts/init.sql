7c2f10d19f87993ca38fcb518725ceaea6b66302
Mika Vakula <mika@vakula.fi>
2018-08-24 01:57:17 +0300

    Delete all related pings when endpoint is deleted

--- scripts/init.sql
+++ scripts/init.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS endpoints (
 
 CREATE TABLE IF NOT EXISTS pings (
   id serial PRIMARY KEY,
-  endpoint_id integer REFERENCES endpoints,
+  endpoint_id integer REFERENCES endpoints ON DELETE CASCADE,
   latency smallint NOT NULL,
   status_code smallint NOT NULL,
   timestamp timestamp DEFAULT current_timestamp
