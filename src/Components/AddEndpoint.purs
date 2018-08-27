3d4760c68343ab661c811da30c5a9bb33d342662
Mika Vakula <mika@vakula.fi>
2018-08-27 20:45:02 +0300

    Refact: Move AddNewEndpoint to own module

---
+++ src/Components/AddEndpoint.purs
@@ -0,0 +1,113 @@
+module AddEndpoint where
+
+import Prelude
+
+import Data.Either (Either(..))
+import Data.Maybe (fromMaybe)
+import Effect.Aff (Aff, attempt, launchAff_)
+import Effect.Class (liftEffect)
+import Effect.Class.Console (logShow)
+import Milkis as M
+import React.Basic (ReactComponent, react)
+import React.Basic.DOM as R
+import React.Basic.DOM.Events as DE
+import React.Basic.Events as Events
+import Simple.JSON (writeJSON)
+import Utils (baseUrl, fetch)
+
+addEndPoint :: ReactComponent { refreshEndpoints' :: Aff Unit }
+addEndPoint = react
+  { displayName: "addNewEndPoint"
+  , initialState
+  , receiveProps
+  , render
+  }
+  where
+    initialState =
+      { name: ""
+      , url: ""
+      }
+    receiveProps _ _ _ = pure unit
+    render props state setState =
+      let
+        handleOnChangeName =
+          Events.handler
+            DE.targetValue
+              \value ->
+                setState _ { name = fromMaybe "" value }
+        handleOnChangeUrl =
+          Events.handler
+            DE.targetValue
+              \value ->
+                setState _ { url = fromMaybe "" value }
+        handleOnSubmit =
+          Events.handler
+            DE.preventDefault $
+              \_ -> launchAff_ do
+                let
+                  body = writeJSON
+                    { name: state.name
+                    , url: state.url
+                    }
+                  opts =
+                    { method: M.postMethod
+                    , body
+                    , headers: M.makeHeaders { "Content-Type": "application/json" }
+                    }
+                res <- attempt $ fetch (M.URL $ baseUrl <> "addEndpoint") opts
+                case res of
+                  Right response -> do
+                    props.refreshEndpoints'
+                    liftEffect $ setState _ { name = "", url = "" }
+                  Left e -> do
+                    logShow e
+      in
+        R.div { children:
+          [ R.h3 { children: [ R.text "Add new endpoint" ] }
+          , R.form
+            { className: "add-endpoint"
+            , onSubmit: handleOnSubmit
+            , children:
+              [ R.div
+                { className: "form-row", children:
+                  [ R.label
+                    { htmlFor: "name"
+                    , children: [ R.text "name" ]
+                    }
+                  , R.input
+                    { type: "text"
+                    , id: "name"
+                    , required: true
+                    , value: state.name
+                    , onChange: handleOnChangeName
+                    }
+                  ]
+                }
+              , R.div
+                { className: "form-row", children:
+                  [ R.label
+                    { htmlFor: "url"
+                    , children: [ R.text "url" ]
+                    }
+                  , R.input
+                    { type: "text"
+                    , id: "url"
+                    , required: true
+                    , value: state.url
+                    , onChange: handleOnChangeUrl
+                    }
+                  ]
+                }
+              , R.div
+                { className: "form-row", children:
+                  [ R.input
+                    { type: "submit"
+                    , className: "submit"
+                    , value: "Submit"
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        }
--- src/Main.purs
+++ src/Main.purs
@@ -2,9 +2,10 @@ module Main where
 
 import Prelude
 
+import AddEndpoint (addEndPoint)
 import Data.Array as Array
 import Data.Either (Either(..))
-import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Maybe (Maybe(..))
 import Effect.Aff (Aff, attempt, launchAff_)
 import Effect.Class (liftEffect)
 import Effect.Class.Console (logShow)
@@ -12,9 +13,7 @@ import Effect.Timer (setInterval)
 import Milkis as M
 import React.Basic (ReactComponent, createElement, react)
 import React.Basic.DOM as R
-import React.Basic.DOM.Events as DE
-import React.Basic.Events as Events
-import Simple.JSON (read, writeJSON)
+import Simple.JSON (read)
 import Status (status)
 import Types (Endpoint, PingData)
 import Utils (baseUrl, fetch)
@@ -46,7 +45,7 @@ main = react { displayName: "Main", initialState, receiveProps, render }
         filterPings id = Array.filter (\p -> p.endpointId == id) state.pings
       in
         R.div { children:
-          [ createElement addNewEndPoint { refreshEndpoints' }
+          [ createElement addEndPoint { refreshEndpoints' }
           ] <> ((\endpoint -> createElement status { endpoint, refreshEndpoints', pings: (filterPings endpoint.id) }) <$> state.endpoints)
         }
 
@@ -101,100 +100,3 @@ getPings = do
     Left e -> do
       logShow e
       pure Nothing
-
-addNewEndPoint :: ReactComponent { refreshEndpoints' :: Aff Unit }
-addNewEndPoint = react
-  { displayName: "addNewEndPoint"
-  , initialState
-  , receiveProps
-  , render
-  }
-  where
-    initialState =
-      { name: ""
-      , url: ""
-      }
-    receiveProps _ _ _ = pure unit
-    render props state setState =
-      let
-        handleOnChangeName =
-          Events.handler
-            DE.targetValue
-              \value ->
-                setState _ { name = fromMaybe "" value }
-        handleOnChangeUrl =
-          Events.handler
-            DE.targetValue
-              \value ->
-                setState _ { url = fromMaybe "" value }
-        handleOnSubmit =
-          Events.handler
-            DE.preventDefault $
-              \_ -> launchAff_ do
-                let
-                  body = writeJSON
-                    { name: state.name
-                    , url: state.url
-                    }
-                  opts =
-                    { method: M.postMethod
-                    , body
-                    , headers: M.makeHeaders { "Content-Type": "application/json" }
-                    }
-                res <- attempt $ fetch (M.URL $ baseUrl <> "addEndpoint") opts
-                case res of
-                  Right response -> do
-                    props.refreshEndpoints'
-                    liftEffect $ setState _ { name = "", url = "" }
-                  Left e -> do
-                    logShow e
-      in
-        R.div { children:
-          [ R.h3 { children: [ R.text "Add new endpoint" ] }
-          , R.form
-            { className: "add-endpoint"
-            , onSubmit: handleOnSubmit
-            , children:
-              [ R.div
-                { className: "form-row", children:
-                  [ R.label
-                    { htmlFor: "name"
-                    , children: [ R.text "name" ]
-                    }
-                  , R.input
-                    { type: "text"
-                    , id: "name"
-                    , required: true
-                    , value: state.name
-                    , onChange: handleOnChangeName
-                    }
-                  ]
-                }
-              , R.div
-                { className: "form-row", children:
-                  [ R.label
-                    { htmlFor: "url"
-                    , children: [ R.text "url" ]
-                    }
-                  , R.input
-                    { type: "text"
-                    , id: "url"
-                    , required: true
-                    , value: state.url
-                    , onChange: handleOnChangeUrl
-                    }
-                  ]
-                }
-              , R.div
-                { className: "form-row", children:
-                  [ R.input
-                    { type: "submit"
-                    , className: "submit"
-                    , value: "Submit"
-                    }
-                  ]
-                }
-              ]
-            }
-          ]
-        }
