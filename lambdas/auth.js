24063fa9b41038bb76a91f35639f584d2fbe6704
Mika Vakula <mika@vakula.fi>
2018-09-10 02:01:54 +0300

    Fix CORS issues

--- lambdas/auth.js
+++ lambdas/auth.js
@@ -3,6 +3,6 @@ const { authString, generatePolicy } = require('../output/Auth')
 module.exports.handler = (event, context, callback) => {
   const authToken = event.authorizationToken
   return authToken == authString
-    ? callback(null, generatePolicy('userId123')('Allow')(event.methodArn))
+    ? callback(null, generatePolicy('user')('Allow')('*'))
     : callback('Unauthorized')
 }
